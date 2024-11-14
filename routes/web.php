3bc56ac6858e3a34f36ed001c53c7c38592b6f6f
John Lyold Lozada <167066974+DeJure12705@users.noreply.github.com>
2024-11-14 21:07:40 +0800

    Squashed commit of the following:
    commit 247ab92573bf4d1806a73bc192b764e823e66259
    Author: John Lyold Lozada <167066974+DeJure12705@users.noreply.github.com>
    Date:   Thu Nov 14 19:36:44 2024 +0800
    
        first commit

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\LockerController;
 use Illuminate\Support\Facades\Route;
 
-//welcome page
+// Welcome page
 Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/adminDashboard', [AdminController::class, 'index'])->name('adminDashboard');
 Route::get('/addLockers', [AdminController::class, 'create'])->name('addLockers');
+Route::get('/viewLockers', [LockerController::class, 'view'])->name('viewLockers');
+Route::get('/assignLockers', [LockerController::class, 'assign'])->name('assignLockers');
+
